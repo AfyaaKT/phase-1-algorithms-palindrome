b504bb56df600d48da8671fa48c091f040a79b94
unknown <afyaakhudur@gmail.com>
2024-04-20 22:45:42 +0300

    complete assynment

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  const lettersArray = word.toLowerCase().split('')
+  
+  console.log('splitting sting into array=> ',lettersArray)
+  const reversedArray = lettersArray.reverse().join('')
+  console.log('reversed Array => ', reversedArray)
+  console.log(word)
+  if(word === reversedArray){
+    return true;
+  }else{
+    return false;
+  }
 }
-
+console.log(isPalindrome('level'))
 /* 
   Add your pseudocode here
+1. reverse the word 
+2.compare the word with its reverse 
+3. if thay are equal return true
+4. if not reverse false
 */
 
 /*
@@ -20,6 +34,16 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("level"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("madam"));
 }
 
 module.exports = isPalindrome;
